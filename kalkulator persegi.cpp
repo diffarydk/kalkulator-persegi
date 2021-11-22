fdc3ebf70710db3e3b1d40ba40ba8655c0193c4b
diffary <94813646+diffarydk@users.noreply.github.com>
2021-11-22 09:25:30 +0700

    Add files via upload

---
+++ kalkulator persegi.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int s,luas,keliling;
+cout <<"PROGRAM MENGHITUNG LUAS DAN KELILING PERSEGI"<<endl;
+
+cout <<"Masukan sisi = ";
+cin >>s;
+
+luas=s*s;
+keliling= 4*s;
+
+cout <<"Luas persegi panjang = "<<luas<<endl;
+cout <<"Keliling persegi panjang = "<<keliling;
+}
